238c2a6527e2c35d335f396614db1c9cb00e6dc2
KepKepKep <vlad.kovalov2000@gmail.com>
2021-09-14 14:54:57 +0300

    FIX

--- DAO/user.ts
+++ DAO/user.ts
@@ -1,47 +1,47 @@
-import {User} from "../db/models/user";
+import { User } from "../db/models/user";
 
 class UserDAO {
 
-    createUser(name, login, hashPassword) {
-        return new User({
-                    name,
-                    login,
-                    password: hashPassword
-              }).save()
+  createUser(name, login, hashPassword) {
+    return new User({
+      name,
+      login,
+      password: hashPassword
+    }).save()
 
-    }
+  }
 
-    getUsers() {
-        return User.find();
-    }
+  getUsers() {
+    return User.find();
+  }
 
-    getOneUser(id) {
-        return User.findById(id);
-    }
+  getOneUser(id) {
+    return User.findById(id);
+  }
 
-    updateUser(id, {name, login, password}) {
-        return User.findOneAndUpdate(
-            {_id: id},
-            {
-                $set: {name, login, password}
-            },
-            {new: true}
-            );
-    }
+  updateUser(id, { name, login, password }) {
+    return User.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: { name, login, password }
+      },
+      { new: true }
+    );
+  }
 
-    deleteUser(id) {
-        return User.deleteOne({id})
-
-
-    }
-
-    loginVerification(login) {
-        return User.findOne({login: login})
+  deleteUser(id) {
+    return User.deleteOne({ id })
 
 
-    }
+  }
+
+  loginVerification(login) {
+    return User.findOne({ login: login })
+
+
+  }
 }
 
 const userDAO = new UserDAO()
 
-export {userDAO}
+export { userDAO }
