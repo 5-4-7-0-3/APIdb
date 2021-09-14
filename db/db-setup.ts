5bd96584d2ac64a997f15b037e235b6630ac263a
KepKepKep <vlad.kovalov2000@gmail.com>
2021-09-14 17:00:52 +0300

    FIX

--- db/db-setup.ts
+++ db/db-setup.ts
@@ -1,10 +1,9 @@
-
 import mongoose from 'mongoose';
 function setupDb() {
     main().catch(err => console.log(err));
 
     async function main() {
-      await mongoose.connect('mongodb+srv://Freak:5t4r3e2w1q@api.hngeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+      await mongoose.connect('mongodb+srv://Freak:5t4r3e2w1q@api.hngeh.mongodb.net/API?retryWrites=true&w=majority');
     }
 }
 
