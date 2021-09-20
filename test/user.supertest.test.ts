ca88b2fa93a342593a397d58dcbc7e4ef69c2216
KepKepKep <vlad.kovalov2000@gmail.com>
2021-09-20 18:50:35 +0300

    Supertest

--- test/user.supertest.test.ts
+++ test/user.supertest.test.ts
@@ -1,6 +1,8 @@
 import app from '../index'
 import supertest from 'supertest'
 import faker from 'faker'
+import bcrypt from 'bcryptjs'
+import { param } from 'express-validator'
 
 const request = supertest(app)
 
@@ -13,8 +15,27 @@ describe('UserRoutes', () => {
 
   it('registration', async () => {
     const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
+    const login = faker.internet.userName()
+    const password = faker.internet.password()
+    const res = await request.post('/user/registration')
+      .send({
+        name,
+        login,
+        password
+      })
+    expect(res.body.name).not.toBe(0)
+    expect(res.body.login).not.toBe(0)
+    expect(res.body.name).not.toBeDefined
+    expect(res.body.login).not.toBeDefined
+    expect(res.body).not.toBeNull()
+    expect(res.body.name).toBe(name)
+    expect(res.body.login).toBe(login)
+  });
+
+  it('registrationError', async () => {
+    const name = "test"
+    const login = "test"
+    const password = "test"
     const res = await request.post('/user/registration')
       .send({
         name,
@@ -22,15 +43,15 @@ describe('UserRoutes', () => {
         password
       })
 
-    expect(res.body).not.toBeNull();
-    expect(res.body.name).toBe(name);
-    expect(res.body.login).toBe(login);
+    expect(res.body.errors[0].msg).toContain('Login password must be more than 5 characters and less than 24')
+    expect(res.body.errors[1].msg).toContain('Password must be more than 5 characters and less than 24')
+
   });
 
   it('auth', async () => {
     const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
+    const login = faker.internet.userName()
+    const password = faker.internet.password()
 
     await request.post('/user/registration')
       .send({
@@ -49,176 +70,155 @@ describe('UserRoutes', () => {
     expect(res.body.refreshToken).not.toBeNull()
   });
 
+  it('authError', async () => {
+    const login: string = "123456789"
+    const password: string = "123456789"
+    const res = await request.post('/user/auth')
+      .send({
+        login,
+        password
+      })
+
+    expect(res.body.message).toContain(`User with this ${login} was not found`)
+  });
+
+  it('authValidationError', async () => {
+    await request.post('/user/registration')
+      .send({
+        name: "testtest",
+        login: "testtest",
+        password: "testtest"
+      })
+
+    const res = await request.post('/user/auth')
+      .send({
+        login: "testtest",
+        password: "testtest1"
+      })
+
+    expect(res.body.message).toContain('Wrong password')
+  });
+
   it('refreshToken', async () => {
-    const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
-
-    const registration = await request.post('/user/registration')
-      .send({
-        name,
-        login,
-        password
-      })
-    const user_id = registration.body._id
-    const auth = await request.post('/user/auth')
-      .send({
-        login,
-        password
-      })
-    const refreshToken = auth.body.refreshToken
+    const { id, refreshToken } = await auth()
     const res = await request.post('/user/auth/refresh')
-      .send({ user_id, refreshToken })
+      .send({ id, refreshToken })
 
+    expect(res.body).toMatchObject({})
     expect(res.body.accessToken).not.toBeNull()
     expect(res.body.refreshToken).not.toBeNull()
+  });
 
+  it('refreshTokenError', async () => {
+    const { id } = await auth()
+    const refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    const res = await request.post('/user/auth/refresh')
+      .send({ id, refreshToken })
+
+    expect(res.body.message).toContain('invalid token')
   });
 
   it('createUser', async () => {
-    const name = faker.name.firstName()
-    const login = faker.name.firstName()
+    const { name, login } = await auth()
     const res = await request.post('/user')
       .send({
         name,
         login,
-        password: faker.name.firstName(),
+        password: faker.internet.password(),
       })
 
     expect(res.body).not.toBeNull();
     expect(res.body.name).toBe(name);
     expect(res.body.login).toBe(login);
   });
+
+  it('createUserError', async () => {
+    const res = await request.post('/user')
+      .send({
+        name: "test",
+        login: "test",
+        password: faker.internet.password(),
+      })
+
+    expect(res.body.errors[0].msg).toContain('Login password must be more than 5 characters and less than 24')
+  });
+
   it('getUsers', async () => {
-    const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
-
-    await request.post('/user/registration')
-      .send({
-        name,
-        login,
-        password
-      })
-    const auth = await request.post('/user/auth')
-      .send({
-        login,
-        password
-      })
-    const token = auth.body.accessToken
-
+    const { accesstoken } = await auth()
     const res = await request.get('/user')
-      .auth(token, { type: "bearer" })
+      .auth(accesstoken, { type: "bearer" })
 
     expect(res.body).not.toBeNull();
   });
 
   it('getOneUser', async () => {
-    const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
-
-    const registration = await request.post('/user/registration')
-      .send({
-        name,
-        login,
-        password
-      })
-    const id = registration.body._id
-    const auth = await request.post('/user/auth')
-      .send({
-        login,
-        password
-      })
-    const token = auth.body.accessToken
-
+    const { id, accesstoken } = await auth()
     const res = await request.get(`/user/${id}`)
-      .auth(token, { type: "bearer" })
+      .auth(accesstoken, { type: "bearer" })
 
     expect(res.body._id).toBe(id);
     expect(res.body).not.toBeNull();
   });
 
   it('updateUser', async () => {
-    let name: string = faker.name.firstName()
-    let login: string = faker.name.firstName()
-    let password: string = faker.name.firstName()
-    let updateName
-    let updateLogin
-    let updatePassword
-
-    const registration = await request.post('/user/registration')
-      .send({
-        name,
-        login,
-        password
-      })
-    const id = registration.body._id
-
-    const auth = await request.post('/user/auth')
-      .send({
-        login,
-        password
-      })
-    const token = auth.body.accessToken
-
-    if (Boolean(Math.round(Math.random()))) {
-      name = null
-    } else {
-      name = faker.name.firstName();
-    }
-
-    if (Boolean(Math.round(Math.random()))) {
-      login = null
-    } else {
-      login = faker.name.firstName();
-    }
-
-    if (Boolean(Math.round(Math.random()))) {
-      password = null
-    } else {
-      password = faker.name.firstName();
-    }
+    const { id, accesstoken } = await auth()
+    let updateName: string = faker.name.firstName()
+    let updateLogin: string = faker.internet.userName()
+    let updatePassword: string = faker.internet.password()
 
     const res = await request.put(`/user/${id}`)
-      .auth(token, { type: "bearer" })
-      .send({ name, login, password })
+      .auth(accesstoken, { type: "bearer" })
+      .send({ name: updateName, login: updateLogin, password: updatePassword })
+    const hashPassword = bcrypt.compareSync(updatePassword, res.body.password)
 
-    if (updateName != null) {
-      expect(res.body.name).toBe(updateName);
-    }
-    if (updateLogin != null) {
-      expect(res.body.login).toBe(updateLogin);
-    }
-    if (updatePassword != null) {
-      expect(res.body.password).toBe(updatePassword);
-    }
-
+    expect(res.body.name).toBe(updateName);
+    expect(res.body.login).toBe(updateLogin);
+    expect(hashPassword).toBeTruthy()
     expect(res).not.toBeNull();
   });
 
-  it('deleteUser', async () => {
-    const name = faker.name.firstName()
-    const login = faker.name.firstName()
-    const password = faker.name.firstName()
+  it('updateUserError', async () => {
+    const { id, accesstoken } = await auth()
+    const res = await request.put(`/user/${id}`)
+      .auth(accesstoken, { type: "bearer" })
+      .send({
+        name: "test",
+        login: "test",
+        password: "test"
+      })
+      expect(res.body.errors[0].msg).toContain('Login password must be more than 5 characters and less than 24')
+      expect(res.body.errors[1].msg).toContain('Password must be more than 5 characters and less than 24')
+  });
 
-    const registration = await request.post('/user/registration')
-      .send({
-        name,
-        login,
-        password
-      })
-    const _id = registration.body._id
-    const auth = await request.post('/user/auth')
-      .send({
-        login,
-        password
-      })
-    const token = auth.body.accessToken
-    const res = await request.delete(`/user/${_id}`)
-      .auth(token, { type: "bearer" })
+  it('deleteUser', async () => {
+    const { id, accesstoken } = await auth()
+    const res = await request.delete(`/user/${id}`)
+      .auth(accesstoken, { type: "bearer" })
 
     expect(res.body).toEqual({ deletedCount: 1 });
   });
 });
 
+async function auth() {
+  let name: string = faker.name.firstName()
+  let login: string = faker.internet.userName()
+  let password: string = faker.internet.password()
+
+  const registration = await request.post('/user/registration')
+    .send({
+      name,
+      login,
+      password
+    })
+
+  const id: any = registration.body._id
+  const auth = await request.post('/user/auth')
+    .send({
+      login,
+      password
+    })
+
+  const accesstoken: any = auth.body.accessToken
+  const refreshToken: any = auth.body.refreshToken
+  return ({ id, name, login, password, accesstoken, refreshToken })
+}
