37e6c46adfd2fc40bb223f72e63e65bb46a4e699
KepKepKep <vlad.kovalov2000@gmail.com>
2021-09-27 16:19:37 +0300

    eslint and prettier

--- client.ts
+++ client.ts
@@ -2,9 +2,7 @@ import io from "socket.io-client";
 
 const socket = io("ws://localhost:3000");
 
-
 socket.on("notification", (arg1: any, arg2: any) => {
     console.log(arg1);
-    console.log(arg2)
+    console.log(arg2);
 });
-
