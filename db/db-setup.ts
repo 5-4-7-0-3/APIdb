d2498e354cfadca2bca8c29cdb481ec22ad18cd6
KepKepKep <vlad.kovalov2000@gmail.com>
2021-09-28 16:42:28 +0300

    fix

--- db/db-setup.ts
+++ db/db-setup.ts
@@ -7,15 +7,17 @@ import {
     MONGO_USERNAME,
     options,
 } from "./config";
+let url: string;
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
+if (MONGO_DB != undefined) {
+    url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+} else {
+    url = `mongodb+srv://Freak:5t4r3e2w1q@api.hngeh.mongodb.net/test?authSource=admin&replicaSet=atlas-5z5tvk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
+}
 function setupDb() {
     main().catch((err) => console.log(err));
 
     async function main() {
-        console.log(url);
-
         await mongoose.connect(url, options);
         console.log("MongoDB is connected");
     }
