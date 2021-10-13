bebd8e1c868ad5afb42a995d4093d4de028828f6
KepKepKep <vlad.kovalov2000@gmail.com>
2021-10-13 13:55:14 +0300

    FS

--- FS practicing/fstest.js
+++ FS practicing/fstest.js
@@ -1,33 +1,37 @@
-import fs from "fs";
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import fs from "fs/promises";
 
-fs.readFile("./files/text.txt", function (err, data) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data[0]);
-    }
-});
+// fs.readFile("./files/text.txt", function (err, data) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(data[0]);
+//     }
+// });
 
-fs.readFile("./files/text.txt", function (err, data) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data.toJSON());
-    }
-});
+// fs.readFile("./files/text.txt", function (err, data) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(data.toJSON());
+//     }
+// });
 
-fs.readFile("./files/text.txt", function (err, data) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data.toString());
-    }
-});
+// fs.readFile("./files/text.txt", function (err, data) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(data.toString());
+//     }
+// });
 
-fs.stat("./files/text.txt", function (err, stats) {
-    console.log(stats.isFile());
-    console.log(stats);
-});
+// fs.stat("./files/text.txt", function (err, stats) {
+//     console.log(stats.isFile());
+//     console.log(stats);
+// });
 
 // fs.writeFile("./files/text.txt", "data", function(err){
 //     if(err)throw err;
@@ -47,3 +51,36 @@ fs.stat("./files/text.txt", function (err, stats) {
 //             console.log(data[0]);
 //         }
 //     };
+
+////////////
+let filehandle;
+try {
+    filehandle = await fs.readFile("./files/text.txt");
+    console.log(filehandle);
+} catch (err) {
+    console.error(console.error(err));
+}
+
+// try {
+//     await fs.unlink("./files/text.txt");
+//     console.log("successfully deleted ./files/text.txt");
+// } catch (error) {
+//     console.error("there was an error:", error.message);
+// }
+
+try {
+    filehandle = await fs.readFile("./files/text.txt");
+    console.log(filehandle);
+} finally {
+    // await filehandle?.close();
+}
+try {
+    filehandle = await fs.writeFile("./files/text.txt", "data");
+    console.log(filehandle);
+    filehandle = await fs.rename("./files/text.txt", "./files/new.txt");
+    console.log(filehandle);
+    filehandle = await fs.unlink("./files/new.txt");
+    console.log(filehandle);
+} catch (err) {
+    console.error(console.error(err));
+}
